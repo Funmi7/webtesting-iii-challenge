0cae9782f924437115280e0b52d46657f7090268
Funmi7 <talabifunmilayo@gmail.com>
2019-10-14 14:22:45 +0100

    Added tests to the Dashboard component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,6 @@
 // Test away!
+// import react from 'react';
+// import * as rtl from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import Display from './Display';
+
